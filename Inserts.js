0fbe78730bb397ac8c5b3c6d58ddea5048a9c812
Alexander Kersten <alexanderkersten@users.noreply.github.com>
2019-01-24 12:31:19 +0100

    Update Inserts.js

--- Inserts.js
+++ Inserts.js
@@ -1,14 +1,29 @@
 // Asegurar que no hay alumnos en la colección
 db.alumno.drop();
 
+// Lista con nombres y apellidos
+var nombres = ["Pepe", "Juan", "Jose", "Jordi", "Maria"];
+var apellidos = ["Fernandez", "Hernandez", "Pujol", "Rico", "Sanchez"];
+
 // Crear 1000 alumnos con los campos: IdAlumno, Nombre, Apellidos y Dni
 for (i = 1; i <= 1000; i++) {
+	var nombre = nombres[Math.floor(Math.random() * nombres.length)];
+	var apellido1 = apellidos[Math.floor(Math.random() * apellidos.length)];
+	var apellido2 = apellidos[Math.floor(Math.random() * apellidos.length)];
+	var dni = "";
+	
+	for (j = 1; j <= 8; j++) {
+		dni += Math.floor(Math.random() * 10);
+	}
+	
+	dni += String.fromCharCode(65 + Math.floor(Math.random() * 26));;
+	
 	db.alumno.insertOne(
 		{
 			IdAlumno: NumberInt(i),
-			Nombre: "Hola",
-			Apellidos: "Mundo",
-			Dni: "12345678A"
+			Nombre: nombre,
+			Apellidos: apellido1 + " " + apellido2,
+			Dni: dni
 		}
 	);
 }
