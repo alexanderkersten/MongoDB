d289e775364ac49de75b20e9a28401aba61cc0be
Alexander Kersten <alexanderkersten@users.noreply.github.com>
2019-01-25 11:59:03 +0100

    Update crud.js

--- crud.js
+++ crud.js
@@ -1,52 +1,56 @@
+// Crear función findAlumno
 db.system.js.save(
-	{
-		_id: "findAlumno",
-		value: function(dni) {
-			return db.alumno.find({Dni: dni});
-		}
-	}
+    {
+        _id: "findAlumno",
+        value: function(dni) {
+            return db.alumno.find( { Dni: dni } );
+        }
+    }
 );
 
+// Crear función insertAlumno
 db.system.js.save(
-	{
-		_id: "insertAlumno",
-		value: function(dni, nombre, apellido1, apellido2) {
-			var alumnos = db.alumno.find().sort({IdAlumno: -1});
-			
-			db.alumno.insertOne(
-				{
-					IdAlumno: NumberInt(alumnos[0].IdAlumno + 1),
-					Dni: dni,
-					Nombre: nombre,
-					Apellidos: apellido1 + " " + apellido2
-				}
-			);
-		}
-	}
+    {
+        _id: "insertAlumno",
+        value: function(dni, nombre, apellido1, apellido2) {
+            var alumnos = db.alumno.find().sort( { IdAlumno: -1 } );
+            
+            db.alumno.insertOne(
+                {
+                    IdAlumno: NumberInt(alumnos[0].IdAlumno + 1),
+                    Dni: dni,
+                    Nombre: nombre,
+                    Apellidos: apellido1 + " " + apellido2
+                }
+            );
+        }
+    }
 );
 
+// Crear función updateAlumno
 db.system.js.save(
-	{
-		_id: "updateAlumno",
-		value: function(dni, nombre, apellido1, apellido2) {			
-			db.alumno.updateOne(
-				{ Dni: dni },
-				{ $set: {
-					Nombre: nombre,
-					Apellidos: apellido1 + " " + apellido2
-				} }
-			);
-		}
-	}
+    {
+        _id: "updateAlumno",
+        value: function(dni, nombre, apellido1, apellido2) {            
+            db.alumno.updateOne(
+                { Dni: dni },
+                { $set: {
+                    Nombre: nombre,
+                    Apellidos: apellido1 + " " + apellido2
+                } }
+            );
+        }
+    }
 );
 
+// Crear función deleteAlumno
 db.system.js.save(
-	{
-		_id: "deleteAlumno",
-		value: function(dni) {
-			db.alumno.deleteOne(
-				{ Dni: dni }
-			);
-		}
-	}
+    {
+        _id: "deleteAlumno",
+        value: function(dni) {
+            db.alumno.deleteOne(
+                { Dni: dni }
+            );
+        }
+    }
 );
