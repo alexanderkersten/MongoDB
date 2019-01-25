1796d4be90d11f61c06bf130eaa91de1cdb711b2
Alexander Kersten <alexanderkersten@users.noreply.github.com>
2019-01-25 11:17:09 +0100

    Add files via upload

---
+++ crud.js
@@ -0,0 +1,52 @@
+db.system.js.save(
+	{
+		_id: "findAlumno",
+		value: function(dni) {
+			return db.alumno.find({Dni: dni});
+		}
+	}
+);
+
+db.system.js.save(
+	{
+		_id: "insertAlumno",
+		value: function(dni, nombre, apellido1, apellido2) {
+			var alumnos = db.alumno.find().sort({IdAlumno: -1});
+			
+			db.alumno.insertOne(
+				{
+					IdAlumno: NumberInt(alumnos[0].IdAlumno + 1),
+					Dni: dni,
+					Nombre: nombre,
+					Apellidos: apellido1 + " " + apellido2
+				}
+			);
+		}
+	}
+);
+
+db.system.js.save(
+	{
+		_id: "updateAlumno",
+		value: function(dni, nombre, apellido1, apellido2) {			
+			db.alumno.updateOne(
+				{ Dni: dni },
+				{ $set: {
+					Nombre: nombre,
+					Apellidos: apellido1 + " " + apellido2
+				} }
+			);
+		}
+	}
+);
+
+db.system.js.save(
+	{
+		_id: "deleteAlumno",
+		value: function(dni) {
+			db.alumno.deleteOne(
+				{ Dni: dni }
+			);
+		}
+	}
+);
