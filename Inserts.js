1bb4da7b38a690a7da6af7bcd421c90bbbc685d1
Alexander Kersten <alexanderkersten@users.noreply.github.com>
2019-01-25 12:05:14 +0100

    Update Inserts.js

--- Inserts.js
+++ Inserts.js
@@ -1,4 +1,4 @@
-// Asegurar que no hay alumnos en la colección
+// Asegurar que no hay alumnos en la colecciÃ³n
 db.alumno.drop();
 
 // Lista con nombres y apellidos
@@ -7,25 +7,25 @@ var apellidos = ["Fernandez", "Hernandez", "Pujol", "Rico", "Sanchez"];
 
 // Crear 10000 alumnos con los campos: IdAlumno, Nombre, Apellidos y Dni
 for (i = 1; i <= 10000; i++) {
-	var nombre = nombres[Math.floor(Math.random() * nombres.length)];
-	var apellido1 = apellidos[Math.floor(Math.random() * apellidos.length)];
-	var apellido2 = apellidos[Math.floor(Math.random() * apellidos.length)];
-	var dni = "";
-	
-	for (j = 1; j <= 8; j++) {
-		dni += Math.floor(Math.random() * 10);
-	}
-	
-	dni += String.fromCharCode(65 + Math.floor(Math.random() * 26));;
-	
-	db.alumno.insertOne(
-		{
-			IdAlumno: NumberInt(i),
-			Nombre: nombre,
-			Apellidos: apellido1 + " " + apellido2,
-			Dni: dni
-		}
-	);
+    var nombre = nombres[Math.floor(Math.random() * nombres.length)];
+    var apellido1 = apellidos[Math.floor(Math.random() * apellidos.length)];
+    var apellido2 = apellidos[Math.floor(Math.random() * apellidos.length)];
+    var dni = "";
+    
+    for (j = 1; j <= 8; j++) {
+        dni += Math.floor(Math.random() * 10);
+    }
+    
+    dni += String.fromCharCode(65 + Math.floor(Math.random() * 26));;
+    
+    db.alumno.insertOne(
+        {
+            IdAlumno: NumberInt(i),
+            Nombre: nombre,
+            Apellidos: apellido1 + " " + apellido2,
+            Dni: dni
+        }
+    );
 }
 
 // Comprobar cuantos alumnos hay
